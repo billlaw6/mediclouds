f004fb6fa389acc6496353bcfcf62af5c06c4c83
billlaw6 <liubin6@gmail.com>
2019-11-06 09:57:24 +0800

    2019-11-06 before eject

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AppState } from './store';
 import { addCount } from './store/actions';
 import './App.css';
 
+// 定义本Component创建时需要的参数类型
 interface IAppProps {
     count: number;
     addCount: typeof addCount;
@@ -25,11 +26,13 @@ class App extends React.Component<IAppProps, {}> {
     };
 }
 
+// 从state里摘出本Component需要的部分作为本Component构建的参数
+// state初始值在reducers里定义了
 const mapStateToProps = (state: AppState) => ({
     count: state.count,
 });
 
-// export default App;
+// 向Component传入参数并构建对象
 export default connect(
     mapStateToProps,
     {
